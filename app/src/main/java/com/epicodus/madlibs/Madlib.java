946aae8aaae423e6b73ea2d6726cf2d6a6254a3b
trevorelvey <trevorelvey@gmail.com>
2016-04-19 09:50:38 -0700

    create Madlib activity

---
+++ app/src/main/java/com/epicodus/madlibs/Madlib.java
@@ -0,0 +1,13 @@
+package com.epicodus.madlibs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Madlib extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_madlib);
+    }
+}
