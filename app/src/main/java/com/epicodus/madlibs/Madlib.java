4a39883efbfc675b2cd2fad255faf39c47344e9b
trevorelvey <trevorelvey@gmail.com>
2016-04-19 15:49:44 -0700

    functioning 'go back' button

--- app/src/main/java/com/epicodus/madlibs/Madlib.java
+++ app/src/main/java/com/epicodus/madlibs/Madlib.java
@@ -3,10 +3,13 @@ package com.epicodus.madlibs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Madlib extends AppCompatActivity {
     private TextView mMadlibTextView;
+    private Button mStartOverButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,14 @@ public class Madlib extends AppCompatActivity {
         String verb = intent.getStringExtra("verb");
         String adjective = intent.getStringExtra("adjective");
         mMadlibTextView.setText("The " + adjective + " " + noun3 + " and the " + noun1 + " " + verb + " the " + noun2 + ", as they do...");
+
+        mStartOverButton = (Button) findViewById(R.id.startOverButton);
+            mStartOverButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick (View v) {
+                    Intent intent = new Intent(Madlib.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
     }
 }
